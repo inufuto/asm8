c552be277d39e16b3b5baa1dfa8acce7cd9e19e0
いぬふと <mxb03523@nifty.com>
2022-05-14 03:17:31 +0900

    Asm86追加

---
+++ Asm86/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Inu.Language;
+
+namespace Inu.Assembler.I8086
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            var normalArgument = new NormalArgument(args);
+            return new Assembler().Main(normalArgument);
+        }
+    }
+}
