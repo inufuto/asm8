62dc4218703a8887bc2edadf0206cf933397f3dd
いぬふと <mxb03523@nifty.com>
2022-08-28 15:29:36 +0900

    i8080を追加

---
+++ Asm80i/Program.cs
@@ -0,0 +1,13 @@
+﻿using Inu.Language;
+
+namespace Inu.Assembler.I8080
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var normalArgument = new NormalArgument(args);
+            return new Assembler().Main(normalArgument);
+        }
+    }
+}
