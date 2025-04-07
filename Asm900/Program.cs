8ec32282f94b1b101c8bcb786bc7fe31f14fa823
いぬふと <mxb03523@nifty.com>
2025-04-07 22:49:00 +0900

    不要enum削除 Asm900マニュアル追加

--- Asm900/Program.cs
+++ Asm900/Program.cs
@@ -4,12 +4,6 @@ namespace Inu.Assembler.Tlcs900;
 
 internal class Program
 {
-    private enum AddressSize
-    {
-        Max,
-        Min,
-    };
-
     static int Main(string[] args)
     {
         var normalArgument = new NormalArgument(args);
